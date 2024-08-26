e9e583865a392e29f5719563afb66d64b40e1a95
jeff30678 <f112113123@nkust.edu.tw>
2024-08-26 23:39:30 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,8 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/qQLiHdbqMygWdqGY9',
+	'https://forms.gle/7wxa5zcx7DKzgrEa7',
+	'https://forms.gle/QraG3US2TUd7o8Ew7',
+	'https://forms.gle/1gQCXAtiM7Nxvxv77'
 ];
